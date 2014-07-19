2fd629390f9a356571e452f40a879331b6f5f2e5
Fräntz Miccoli <frantz.miccoli@fakedomain.com>
2014-07-19 18:00:22 +0200

    Working "rake data"

--- src/metarticle/conceptgraph/__init__.py
+++ src/metarticle/conceptgraph/__init__.py
@@ -15,8 +15,9 @@ class ConceptGraph:
     def prepare_communities(self):
         self.dendrogram = community.generate_dendrogram(self.g)
         for level in range(len(self.dendrogram)):
-            print "partition at level", level, "is",\
-                community.partition_at_level(self.dendrogram, level)
+            pass 
+            #print "partition at level", level, "is",\
+            #    community.partition_at_level(self.dendrogram, level)
 
     def get_communities_levels_number(self):
         return len(self.dendrogram)
